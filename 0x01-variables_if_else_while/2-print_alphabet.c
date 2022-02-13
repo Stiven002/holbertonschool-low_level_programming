1099a670c434a1b86ad132585f44fd555470fa3f
Stiven002 <stivendelgadoty@gmail.com>
2022-02-13 13:04:23 -0800

    print alphate

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	x = 'a';
+	while (x <= 'z')
+	{
+		putchar(x);
+		++x;
+	}
+	x = 'A';
+	while (x <= 'Z')
+	{
+		putchar(x);
+		++x;
+	}
+	putchar('\n');
+	return (0);
+}
